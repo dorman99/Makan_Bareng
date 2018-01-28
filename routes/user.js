a7132ac74578e7f8e48ab68d1d93e03a3ec85497
dorman99 <hafizh.abdillahhh@gmail.com>
2018-01-28 12:00:26 +0700

    updated validation

--- routes/user.js
+++ routes/user.js
@@ -1,73 +1,77 @@
 var express = require('express')
-var router  = express.Router()
+var router = express.Router()
 const Model = require('../models')
-router.get('/',function(req,res){
+router.get('/', function (req, res) {
     Model.User.findAll({
-        order : [['id','ASC']]
+        order: [['id', 'ASC']]
     })
-    .then(dataUsers=>{
-        res.render('users',{dataUsers})
-        // res.send(dataUser)
-    })
-    .catch(err=>{
-        console.log(err)
-    })
+        .then(dataUsers => {
+            res.render('users', { dataUsers })
+            // res.send(dataUser)
+        })
+        .catch(err => {
+            console.log(err)
+        })
 })
 
-router.get('/add',function(req,res){
-    res.render('addUser',{err:null})
+router.get('/add', function (req, res) {
+    res.render('addUser', { err: null })
 })
 
-router.post('/add',function(req,res){
+router.post('/add', function (req, res) {
     let objCreate = {
-        name    : req.body.name,
-        email   : req.body.email  
+        name: req.body.name,
+        email: req.body.email
     }
     Model.User.create(objCreate)
-    .then(()=>{
-        res.redirect('/users')
-    })
-    .catch(err=>{
-        console.log(err.message)
-        res.render('addUser', { err: err})
-    })
+        .then(() => {
+            res.redirect('/users')
+        })
+        .catch(err => {
+            console.log(err.message)
+            res.render('addUser', { err: err })
+        })
 })
 
-router.get('/edit/:id',function(req,res){
-    Model.User.findOne({where : {id : req.params.id}})
-    .then(dataUser=>{
-        res.render('editUser',{dataUser:dataUser,err:null})
-    })
-    .catch(err=>{
-        console.log(err)
-    })
+router.get('/edit/:id', function (req, res) {
+    Model.User.findOne({ where: { id: req.params.id } })
+        .then(dataUser => {
+            res.render('editUser', { dataUser: dataUser, err: null })
+        })
+        .catch(err => {
+            console.log(err)
+        })
 })
 
-router.post('/edit/:id',function(req,res){
+router.post('/edit/:id', function (req, res) {
     let objEdit = {
-        name     : req.body.name,
-        email    : req.body.email
+        id : req.params.id,
+        name: req.body.name,
+        email: req.body.email
     }
 
-    Model.User.update(objEdit,{
-        where : {
-            id : req.params.id
+    Model.User.update(objEdit, {
+        where: {
+            id: req.params.id
         }
-    }).then(()=>{
+    }).then(() => {
         res.redirect('/users')
-    }).catch(err=>{
-        console.log(err)
+    }).catch(err => {
+        Model.User.findOne({ where: { id: req.params.id } })
+        .then(dataUser => {
+            res.render('editUser', { dataUser: dataUser, err: err })
+        })
     })
 })
 
-router.get('/delete/:id',function(req,res){
+router.get('/delete/:id', function (req, res) {
     Model.User.destroy({
-        where : {
-            id : req.params.id
+        where: {
+            id: req.params.id
         }
-    }).then(()=>{
+    }).then(() => {
         res.redirect('/users')
-    }).catch(err=>{
+    }).catch(err => {
         res.send(err)
     })
 })
